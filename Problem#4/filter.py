2e2371cc5a9dd05719bc51254107a9801b180144
Randomsnowman2005 <mohdali.fakhoury@gmail.com>
2024-03-07 14:51:00 -0600

    prob 4 fixed

--- Problem#4/filter.py
+++ Problem#4/filter.py
@@ -1,27 +1,19 @@
 # Mohamad Ali fakhoury @ Thurs 2pm
 def process_and_print(input_string):
 
-    inputs = input_string.split()
-    input_data = [int(token) for token in inputs]
-    # Filter out negative values
-    input_data = [num for num in input_data if num < 0]
-    # Sort integers in reverse order
-    input_data.sort(reverse=True)
-    # Print sorted integers
-    x=0
-    output_string = ''
-    for x in range(len(input_data)):
-        output_string = output_string + str(input_data[x]) + ' '
-    print(output_string)
+ tokens = input_string.split()
+ input_data = []
+ for token in tokens:
+    if int(token) < 0:
+        input_data.append(int(token))
+ input_data.sort(reverse=True)
+ for values in input_data:
+    print(values, end=' ')
 
- 
 
-if __name__ == "__main__":
-
-    # User inputs string w/ numbers
-    user_input = input("Enter a space-separated string of numbers: ")
-    # Call the function to process and print the result
-    process_and_print(user_input)
+if __name__ == "__main__": # User inputs string w/ numbers
+ user_input = input("Enter a space-separated string of numbers: ") # Call the function to process and print the result
+ process_and_print(user_input)
 
 
     
