781e0e5f66e15063c09f3bd207b693d881507fad
Randomsnowman2005 <mohdali.fakhoury@gmail.com>
2024-03-07 14:38:58 -0600

    prob 5 fixed, prob 4 still not working

--- Problem#4/filter.py
+++ Problem#4/filter.py
@@ -1,42 +1,28 @@
 # Mohamad Ali fakhoury @ Thurs 2pm
 def process_and_print(input_string):
+
     inputs = input_string.split()
-    x = 0
-    while x < len(inputs):
-     z = int(inputs[x])
-     inputs[x] = z
-     x = x+1 
-    else:
-     inputs.sort(reverse = True)
-     x= 0
-     fin = []
-     while x < len(inputs):
-        if x == len(inputs)-1:
-          if inputs[x]<0:
-            fin.append(str(inputs[x]))
-            x=x+1
-          else:
-            x=x+1
-        elif inputs[x] < 0 and inputs[x+1] >= 0:
-         fin.append(str(inputs[x]))
-         x = x+1
-         z =z+1
-        elif inputs[x] < 0 and inputs[x+1]<0:
-         fin.append(str(inputs[x]))
-         x = x+1 
-         z=z+1
-        else:
-          x=x+1
-     else:
-        final = ' '.join(fin)
-        print(f'{final} ' )
-        
-   
-    
+    input_data = [int(token) for token in inputs]
+    # Filter out negative values
+    input_data = [num for num in input_data if num < 0]
+    # Sort integers in reverse order
+    input_data.sort(reverse=True)
+    # Print sorted integers
+    x=0
+    output_string = ''
+    for x in range(len(input_data)):
+        output_string = output_string + str(input_data[x]) + ' '
+    print(output_string)
+
+ 
 
 if __name__ == "__main__":
+
     # User inputs string w/ numbers
     user_input = input("Enter a space-separated string of numbers: ")
-
     # Call the function to process and print the result
     process_and_print(user_input)
+
+
+    
+
--- Problem#5/elementsrange.py
+++ Problem#5/elementsrange.py
@@ -1,33 +1,16 @@
 # Mohamad Ali fakhoury @ Thurs @ 2pm
 def filter_and_print_range(input_list, min_val, max_val):
-    x=0
-    fin = ''
-    while x < len(input_list):
-     z = int(input_list[x])
-     input_list[x] = z
-     x = x+1 
-    else:
-     x=-1
-     while x < len(input_list)-1:
-      if input_list[x+1]>=int(min_val) and input_list[x+1]<=int(max_val):
-        fin= fin+(str(input_list[x+1]))+','
-        x=x+1
-      else:
-        x=x+1
-     else:
-       print(fin)
+      for num in input_list:
+        if min_val <= num <= max_val:
+            print(num, end=",")
 
         
 
 if __name__ == '__main__':
-    # Get input for the list of integers
     user_input = input("Enter a space-separated string of numbers: ")
-    integer_list = user_input.split()
-
-    # Get input for the range (min and max values)
+    integer_list = [int(a) for a in user_input.split()]
     user_input = input("Enter the min and max values separated by a space: ")
-    min_val, max_val = user_input.split()
-    filter_and_print_range(integer_list,min_val,max_val)
+    min_val, max_val = map(int, user_input.split())
+    filter_and_print_range(integer_list, min_val, max_val)
 
-    # Call the function to filter and print the numbers in the given range
    
